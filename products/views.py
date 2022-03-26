78edce890cedd12836acb85103d931996592e104
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-26 15:49:10 -0300

    feat: resolve some api issues

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins, permissions
+from rest_framework import generics, permissions
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -29,7 +29,6 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [permissions.DjangoModelPermissions]
-    lookup_field = 'pk'
 
     def perform_update(self, serializer):
         instance = serializer.save()
@@ -42,7 +41,6 @@ product_update_view = ProductUpdateAPIView.as_view()
 class ProductDestroyAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    lookup_field = 'pk'
 
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
