ba63c689f4b74b84225ffa6aa28fc33b980f9923
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-26 13:54:35 -0300

    chore: adapt product urls logics

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+def upload_image_book(instance, filename):
+  return f"{instance.id_product}-{filename}"
+
+class Product(models.Model):
+    id_product = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=120)
+    description = models.CharField(max_length=120)
+    validate_day = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=upload_image_book, blank=True, null=True)
+    price = models.DecimalField(max_digits=20, decimal_places=2, default=00.00)
+    
+    @property
+    def sale_price(self):
+        return "%.2f" %(float(self.price) * 0.8)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, permissions
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        name = serializer.validated_data.get('name')
+        description = serializer.validated_data.get('description') or None
+        if description is None:
+            description = name
+        serializer.save(description=description)
+
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.DjangoModelPermissions]
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.description:
+            instance.description = instance.name
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+
+product_destroy_view = ProductDestroyAPIView.as_view()
